5ac9de611ce8de6166c9e86b4deb480779c7d323
Ken Ramandha <93147426+KenRamandha@users.noreply.github.com>
2025-05-09 15:04:26 +0700

    Presentation Layer

---
+++ composeApp/src/commonMain/kotlin/org/kenramandha/crypto_apps/coins/presentation/UiCoinListItem.kt
@@ -0,0 +1,11 @@
+package org.kenramandha.crypto_apps.coins.presentation
+
+data class UiCoinListItem (
+    val id: String,
+    val name: String,
+    val symbol: String,
+    val iconUrl: String,
+    val formattedPrice: String,
+    val formattedChange: String,
+    val isPositive: Boolean,
+)
